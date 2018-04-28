c6ad19b25cda2c5e179358ccb3e20ed3a05ed128
Alexander Melo <alexandermelox@gmail.com>
2018-04-28 16:21:15 -0400

    added about section

--- frontend/src/components/Landing/Landing.js
+++ frontend/src/components/Landing/Landing.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import classes from './Landing.css';
 
+import SubmitButton from './SubmitButton/SubmitButton';
+
 const landing = () => {
   const styles = {
     color: 'var(--color-primary)',
     lineHeight: 1.4
+  }
+
+  const iconStyles = {
+    fontSize: '5rem',
+    color: 'white',
+    backgroundColor: 'var(--color-primary)',
+    width: '100px',
+    height: '100px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: '50%',
+    marginBottom: '3rem'
   }
 
   return (
@@ -20,7 +35,7 @@ const landing = () => {
             <div className="input-group mb-3 input-group-lg">
               <input type="text" className="form-control " placeholder="Enter your zipcode..." style={{fontSize: '2rem'}} />
               <div className="input-group-append">
-                <button className="btn btn-info" type="button"><i style={{fontSize: '2rem'}} className="fas fa-search"></i></button>
+                <SubmitButton />
               </div>
             </div>
           </div>
@@ -28,12 +43,31 @@ const landing = () => {
       </div>
     </section>
 
-    <section className="about">
+    <section className={classes.About}>
       <div className="container">
         <div className="row">
           <div className="col text-center">
             <h2>Fast, Free, and Secure.</h2>
             <p className={classes.Lead}>3.5 million+ people and counting have used LinkNYC, the world’s largest and fastest free municipal Wi-Fi network.</p>
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-4">
+            <i style={iconStyles} className="fas fa-wifi"></i>
+            <h3>Broadband</h3>
+            <p>OneNYC recognized the internet as a prerequisite for full participation in the city’s economic and civic life and established the goal of making sure every New Yorker has affordable, high-speed internet access by 2025.</p>
+          </div>
+          <div className="col-lg-4">
+            <i style={iconStyles} className="fas fa-wifi"></i>
+            <h3>Broadband</h3>
+            <p>OneNYC recognized the internet as a prerequisite for full participation in the city’s economic and civic life and established the goal of making sure every New Yorker has affordable, high-speed internet access by 2025.</p>
+          </div>
+          <div className="col-lg-4">
+            <i style={iconStyles} className="fas fa-wifi"></i>
+            <h3>Broadband</h3>
+            <p>OneNYC recognized the internet as a prerequisite for full participation in the city’s economic and civic life and established the goal of making sure every New Yorker has affordable, high-speed internet access by 2025.</p>
           </div>
         </div>
       </div>
---
+++ frontend/src/components/Landing/SubmitButton/SubmitButton.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const submitButton = () => {
+  return (
+    <button className="btn btn-info" type="button"><i style={{fontSize: '2rem'}} className="fas fa-search"></i></button>
+  )
+}
+
+export default submitButton;
