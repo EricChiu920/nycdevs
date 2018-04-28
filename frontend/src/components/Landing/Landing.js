9d9581410ef36e760dcf40d908a90afa45037973
Alexander Melo <alexandermelox@gmail.com>
2018-04-28 11:59:56 -0400

    added landing styles

--- frontend/src/components/Landing/Landing.js
+++ frontend/src/components/Landing/Landing.js
@@ -4,6 +4,9 @@ import classes from './Landing.css';
 const landing = () => (
   <section className={classes.Hero}>
     <div className="container">
+      <div className={classes.Logo}>
+        <span className={classes.Logo__text}>wifi.pls</span>
+      </div>
       <div className="row">
         <div className="col-md-8 mx-auto">
             fsdf
